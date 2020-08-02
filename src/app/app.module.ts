198cade0e991231eb3fa884392db371c3f7f0289
dmester <daniel@mester.se>
2020-08-02 11:13:56 +0200

    Added sample code for custom icon design.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgxJdenticonModule } from 'ngx-jdenticon';
+import { NgxJdenticonModule, JDENTICON_CONFIG } from 'ngx-jdenticon';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
@@ -16,7 +16,24 @@ import { UserComponent } from './user.component';
     NgxJdenticonModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      // Custom identicon style
+      // https://jdenticon.com/icon-designer.html?config=222222ff014132321e363f52
+      provide: JDENTICON_CONFIG,
+      useValue: {
+        lightness: {
+            color: [0.31, 0.54],
+            grayscale: [0.63, 0.82],
+        },
+        saturation: {
+            color: 0.50,
+            grayscale: 0.50,
+        },
+        backColor: '#222',
+      },
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
